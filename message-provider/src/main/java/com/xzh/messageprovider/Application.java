f81830f3acba202ed3a36fe24c5da847a12be189
xzh <anenan@qq.com>
2019-05-17 14:37:49 +0800

    first commit

---
+++ message-provider/src/main/java/com/xzh/messageprovider/Application.java
@@ -0,0 +1,17 @@
+package com.xzh.messageprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author 向振华
+ * @date 2019/05/08 11:00
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
